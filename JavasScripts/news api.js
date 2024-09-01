a5956c98218fe7a46aa89a9cad4f2c4bdd174ce9
Alan46862546 <alan46862546@edu.pa.senac.br>
2024-09-01 13:00:57 -0300

    Retirada de 2 card, acrescentado codigo da api comentado.

--- JavasScripts/news api.js
+++ JavasScripts/news api.js
@@ -7,3 +7,47 @@ const url = 'https://newsapi.org/v2/everything?' +
     
 var req = new Request(url);
     console.log(url)
+
+    // async function getData() {
+    //     try {
+    //         const response = await fetch(url);
+    //         if (!response.ok) {
+    //             throw new Error(`Response status: ${response.status}`);
+    //         }
+    //         const json = await response.json();
+    //         console.log(json);
+    
+    
+    //         const Cards = document.getElementById("cards_item")
+    //         console.log(json.articles)
+
+    //         json.articles.forEach(item => {
+    //                 const art = document.createElement("card_item");
+    
+    //                 const img = document.createElement("card_image");
+    //                 img.innerHTML = item.urlToImage;
+    //                 art.appendChild(img);
+    
+    //                 const description = document.createElement("card_text");
+    //                 description.innerHTML = item.description;
+    //                 art.appendChild(description);
+    
+    //                 const name = document.createElement("card_title");
+    //                 name.innerHTML = item.name;
+    //                 art.appendChild(name);
+    
+    //                 const url = document.createElement("bnt");
+    //                 url.innerHTML = item.url;
+    //                 art.appendChild(url);
+    
+    //             console.log(art)
+    
+    //         Cards.appendChild(art)
+    //         });
+    
+    //     } catch (error) {
+    //         console.error(error.message);
+    //     }
+    // }
+    
+    // getData()
