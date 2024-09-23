0982eddf872ca83fec4440c17c4dfb2109a67906
Sr-Estronccio <brunojcduarte@gmail.com>
2024-09-23 08:17:37 -0300

    Ajuste de ids no Firebase cad

--- JavasScripts/Firebase cad.js
+++ JavasScripts/Firebase cad.js
@@ -1,26 +1,26 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD3P3uCkKQfpeCDj-5J4cLFTrF3EQk_3fw",
-    authDomain: "copverde-b2567.firebaseapp.com",
-    projectId: "copverde-b2567",
-    storageBucket: "copverde-b2567.appspot.com",
-    messagingSenderId: "327954834216",
-    appId: "1:327954834216:web:f709337934e8a765fea61d",
-    measurementId: "G-29LJK80PD3"
-  };
-  
-  // Initialize Firebase
+  apiKey: "AIzaSyD3P3uCkKQfpeCDj-5J4cLFTrF3EQk_3fw",
+  authDomain: "copverde-b2567.firebaseapp.com",
+  projectId: "copverde-b2567",
+  storageBucket: "copverde-b2567.appspot.com",
+  messagingSenderId: "327954834216",
+  appId: "1:327954834216:web:f709337934e8a765fea61d",
+  measurementId: "G-29LJK80PD3"
+};
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
 function login() {
-  //firebase.auth().signInWithEmailAndPassword('alanlopes272003@gmail.com','123456')
-  (document.getElementById('email').value, document.getElementById('senha').value)
-  .then(() => {
+  firebase.auth().signInWithEmailAndPassword
+    (document.getElementById('email').value, document.getElementById('senha').value)
+    .then(() => {
       //se tiver cadastro
       window.location.href = "index.html";
-  }).catch(error => {
+    }).catch(error => {
       //o que acontece se errar o email / senha
       alert(error);
-  })
+    })
 }
 
 function cadastrar() {
@@ -31,13 +31,13 @@ function registro() {
   const Nome = document.getElementById('nome').value
   const local = document.getElementById('local').value
   const Email = document.getElementById('Email').value
-  const Celular = document.getElementById('Celular').value
-  const Senha = document.getElementById('Senha').value
-  const Confirme_sua_Senha = document.getElementById('Confirme_sua_Senha').value
+  const Celular = document.getElementById('number').value
+  const Senha = document.getElementById('password').value
+  const Confirme_sua_Senha = document.getElementById('confirmPassword').value
 
-  firebase.auth().createUserWithEmailAndPassword(Nome, local, Email, Celular, Senha, Confirme_sua_Senha
+  firebase.auth().createUserWithEmailAndPassword(Nome, local, Email, number, password, confirmPassword
   ).then(() => {
-   window.location.href = "index.html";
+    window.location.href = "index.html";
   }).catch(error => {
     alert(error);
   });
