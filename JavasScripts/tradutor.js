17368f149f6fd2464a9a526b49c32fe2fc047484
Alan46862546 <alan46862546@edu.pa.senac.br>
2024-10-01 22:50:33 -0300

    Css index, Js hambuguermenu + js Tradutor.
    Houve mudanças no css do index (Nova navbar), js do hambuguer e criação do js tradutor.

---
+++ JavasScripts/tradutor.js
@@ -0,0 +1,7 @@
+window.gtranslateSettings =
+{
+    "default_language": "pt", "detect_browser_language": true,
+    "url_structure": "sub_directory", "languages": ["pt", "en", "es", "de", "fr"],
+    "wrapper_selector": ".gtranslate_wrapper",
+    "horizontal_position": "left", "vertical_position": "bottom"
+}
