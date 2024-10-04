3410566b27797cc72c886ae2ce859f7864f403b2
Alan Monteiro <alan46862546@edu.pa.senac.br>
2024-10-04 11:29:54 -0300

    Edição html e css

--- JavasScripts/tradutor.js
+++
@@ -1,7 +0,0 @@
-window.gtranslateSettings =
-{
-    "default_language": "pt", "detect_browser_language": true,
-    "url_structure": "sub_directory", "languages": ["pt", "en", "es", "de", "fr"],
-    "wrapper_selector": ".gtranslate_wrapper",
-    "horizontal_position": "left", "vertical_position": "bottom"
-}
