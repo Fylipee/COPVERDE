03d4a61f4909250c6807528c0155d62233f9632c
Bruno Duarte <bruno10868206@edu.pa.senac.br>
2024-10-18 08:06:00 -0300

    Deleta duplicata firebase initialize

--- JavasScripts/Firebase cad.js
+++ JavasScripts/Firebase cad.js
@@ -1,16 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyD3P3uCkKQfpeCDj-5J4cLFTrF3EQk_3fw",
-  authDomain: "copverde-b2567.firebaseapp.com",
-  projectId: "copverde-b2567",
-  storageBucket: "copverde-b2567.appspot.com",
-  messagingSenderId: "327954834216",
-  appId: "1:327954834216:web:f709337934e8a765fea61d",
-  measurementId: "G-29LJK80PD3"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-
 //login Gmail
 function loginGmail() {
   const provider = new firebase.auth.GoogleAuthProvider();
