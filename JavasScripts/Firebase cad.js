b439871b8f82ab3f99a192f9624b227501b7fca8
Alan Monteiro <alan46862546@edu.pa.senac.br>
2024-10-23 11:48:42 -0300

    Confirmar a senha no registro, deletar paginas de registro e login antigos,

--- JavasScripts/Firebase cad.js
+++ JavasScripts/Firebase cad.js
@@ -14,29 +14,41 @@ function login() {
       //se tiver cadastro
       window.location.href = "index.html";
     }).catch(error => {
-      //o que acontece se errar o email / senha
+      //o que acontece se errar o email / senha.
       alert(error);
     })
 }
 
 function registro() {
-  const Email = document.getElementById('E-mail').value
-  const Senha = document.getElementById('Senha').value
-  // const confirmarSenha = document.getElementById('confirmar-senha').value;
-  firebase.auth().createUserWithEmailAndPassword(Email, Senha
-  ).then(() => {
-    window.location.href = "index.html";
-  }).catch(error => {
-    alert(error);
-  });
+  const Email = document.getElementById('email').value
+  const Senha = document.getElementById('senha').value
+  const confirmarSenha = document.getElementById('confirmar-senha').value;
+
+  // Verifica se as senhas são iguais.
+  if (Senha !== confirmarSenha) {
+    alert('As senhas não coincidem.');
+    return;
 }
 
-//recuparar senha.
+if (Senha.length < 10) {
+  alert('A senha deve ter pelo menos 10 caracteres.');
+  return;
+}
+
+firebase.auth().createUserWithEmailAndPassword(Email, Senha)
+.then(() => {
+    window.location.href = "index.html";
+})
+.catch(error => {
+    alert(error);
+});
+}
+
+// Recuperar senha.
 function resetSenha() {
-  firebase.auth().sendPasswordResetEmail(document.getElementById('E-mail').value).then(() => {
+  firebase.auth().sendPasswordResetEmail(document.getElementById('email').value).then(() => {
     alert('Email enviado com sucesso');
   }).catch(error => {
     alert('Esqueceu o email, bobo.')
   });
-
 }
