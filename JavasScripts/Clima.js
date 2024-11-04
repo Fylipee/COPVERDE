ef6a10a92a9742d0546719f394cdda7af23fff6c
Bruno Duarte <bruno10868206@edu.pa.senac.br>
2024-11-04 10:46:37 -0300

    Clima.js

---
+++ JavasScripts/Clima.js
@@ -0,0 +1,20 @@
+const url = "https://api.weatherapi.com/v1/current.json?key=44662d83639b40f5960120028240411&q=Belem&aqi=no"
+
+var req = new Request(url);
+async function getData() {
+    try {
+        const response = await fetch(req);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json)
+    }
+
+    catch (error) {
+        console.error(error.message);
+
+    }
+
+}
+getData()
