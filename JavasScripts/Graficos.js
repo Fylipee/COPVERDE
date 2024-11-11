02abb130f4b3f79a51edaa6dd85b31bb35608d26
Sr-Estronccio <brunojcduarte@gmail.com>
2024-11-11 18:54:40 -0300

    Gráfico visível

--- JavasScripts/Graficos.js
+++ JavasScripts/Graficos.js
@@ -1,46 +1,37 @@
-// quickchart-js https://github.com/typpo/quickchart-js
-// umidade
-const QuickChart = require('quickchart-js');
+var data = [
+  {
+    type: "indicator",
+    mode: "gauge+number+delta",
+    value: 42,
+    title: { text: "Umidade%", font: { size: 24 } },
+    delta: { reference: 55, increasing: { color: "RebeccaPurple" } },
+    gauge: {
+      axis: { range: [null, 100], tickwidth: 1, tickcolor: "darkblue" },
+      bar: { color: "purple" },
+      bgcolor: "white",
+      borderwidth: 2,
+      bordercolor: "gray",
+      steps: [
+        { range: [0, 40], color: `#EDD188` },
+        { range: [40, 50], color: "#A2CA71" },
+        { range: [50, 60], color: "#387F39" },
+        { range: [60,65], color: "red" },
+        { range: [65, 100], color: "black" }
+      ],
+      threshold: {
+        line: { color: "darkgreen", width: 4 },
+        thickness: 0.75,
+        value: 55
+      }
+    }
+  }
+];
 
-const chart = new QuickChart();
+var layout = {
+  width: 500,
+  height: 400,
+  margin: { t: 25, r: 25, l: 25, b: 25 },
+  font: { color: "darkblue", family: "Arial" }
+};
 
-chart.setWidth(500)
-chart.setHeight(300);
-chart.setVersion('2.9.4');
-
-chart.setConfig({
-  type: 'gauge',
-  data: {
-    datasets: [
-      {
-        value: 'humidity',
-        data: [40,50,60,65,100],
-        backgroundColor: ['#EDD188','#A2CA71', '#387F39', 'red', 'black'],
-        borderWidth: 2,
-      },
-    ],
-  },
-  options: {
-    valueLabel: {
-      fontSize: 22,
-      backgroundColor: 'transparent',
-      color: '#000',
-      formatter: function (value, context) {
-        return value + ' %';
-      },
-      bottomMarginPercentage: 10,
-    },
-  },
-});
-
-// Print the chart URL
-console.log(chart.getUrl());
-
-// Get the image...
-const image = await chart.toBinary();
-
-// Or write it to a file
-chart.toFile('chart.png');
-
-// outro grafico
-
+Plotly.newPlot('myDiv', data, layout);
