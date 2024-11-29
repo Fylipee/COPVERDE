8a3ff8461781136c290a5381cbe400d699694817
Alan Monteiro <alan46862546@edu.pa.senac.br>
2024-11-29 11:51:25 -0300

    visualização de dados do usuário e preview de imagens no perfil

--- JavasScripts/profile.js
+++ JavasScripts/profile.js
@@ -45,9 +45,9 @@ function checkUserAndSaveProfile() {
       const phone = phoneElement.value || "Sem telefone";
       const local = localElement.value || "Sem localização";
 
-      saveOrUpdateUserProfile(user, { name, phone, local });
+      saveOrUpdateUserProfile(user, { name, phone, local});
     } else {
-      alert("Usuário não está logado!");
+      console.log('houve algum erro' + error)
     }
   });
 }
@@ -59,6 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
     saveButton.addEventListener("click", (event) => {
       event.preventDefault();
       checkUserAndSaveProfile();
+      closeModal();
     });
   }
 });
@@ -67,28 +68,3 @@ document.addEventListener("DOMContentLoaded", () => {
 // pra fechar ao modal ao clicar em "salvar"//
 //ajustar pra outras páginas.
 //....
-
-
-// firebase.firestore().collection('users').doc(userId).get()
-//             .then(doc => {
-//                 if (doc.exists) {
-//                     const userData = doc.data(); // Obtenha os dados do usuário
-//                     document.getElementById('email_user').innerText = user.email;
-//                     document.getElementById('nome_user').innerText = userData.Nome; // Supondo que 'nome' está no documento
-//                     document.getElementById('local_user').innerText = userData.Local; // Supondo que 'local' está no documento
-//                     document.getElementById('tel_user').innerText = userData.Telefone; // Supondo que 'tel' está no documento
-//                 } else {
-//                     console.log("Nenhum documento encontrado para este usuário.");
-//                 }
-//             })
-//             .catch(error => {
-//                 console.error("Erro ao buscar dados do usuário: ", error);
-//             });
-//     } else {
-//         // Se o usuário não estiver autenticado, exiba mensagens padrão
-//         document.getElementById('email_user').innerText = 'Usuário não autenticado';
-//         document.getElementById('nome_user').innerText = 'Usuário não autenticado';
-//         document.getElementById('local_user').innerText = 'Usuário não autenticado';
-//         document.getElementById('tel_user').innerText = 'Usuário não autenticado';
-//     }
-// });
