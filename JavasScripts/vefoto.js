2d8790e865347f557d7fb1c767ff37b31e1cd588
Fylipee <fylipe46696006@edu.pa.senac.br>
2024-12-11 09:36:23 -0300

    Solução para Imagem de perfil
    Imagem de perfil vinda do autenticador do gmail da pessoa e adição de imagem padrão para quem não logar pelo auth do gmail

--- JavasScripts/vefoto.js
+++ JavasScripts/vefoto.js
@@ -1,53 +1,74 @@
 
-// Função para pré-visualizar a imagem localmente
-function previewImage(event) {
-  const file = event.target.files[0]; 
-  selectedFile = file; 
-  const reader = new FileReader();
-  reader.onload = function() {  
-    const output = document.getElementById("profilePicture");
-    output.src = reader.result;
-  };
+// Inicialize o Firebase e a autenticação
+const auth = firebase.auth();
 
-  reader.readAsDataURL(file);
-}
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // Usuário está logado
+        const photoURL = user.photoURL; // Obtém a URL da foto do usuário
+        const profilePicture = document.getElementById('profilePicture');
+        profilePicture.classList.add('profile-picture')
+        if (photoURL) {
+            profilePicture.src = photoURL; // Define a URL da foto no elemento <img>
+            profilePicture.style.display = 'block'; // Exibe a imagem
+        } else {
+            profilePicture.src = 'assets/images/Fotos_pra_otimização/perfil.webp'; // URL padrão se não houver foto
+            profilePicture.style.display = 'block'; // Exibe a imagem padrão
+        }
+    } else {
+        // Usuário não está logado
+        console.log('Usuário não autenticado');
+    }
+});
+// // Função para pré-visualizar a imagem localmente
+// function previewImage(event) {
+//   const file = event.target.files[0]; 
+//   selectedFile = file; 
+//   const reader = new FileReader();
+//   reader.onload = function() {  
+//     const output = document.getElementById("profilePicture");
+//     output.src = reader.result;
+//   };
 
-firebase.auth().onAuthStateChanged((user) => {
-const userId = firebase.auth().currentUser?.uid;
+//   reader.readAsDataURL(file);
+// }
 
-if (userId) {
-    console.log("Usuário logado com ID:", userId);
-    firebase.firestore().collection('users').doc(userId).get()
-        .then(doc => {
-            if (doc.exists) {
-                const userData = doc.data();
-                console.log("Dados do usuário:", userData);
-                document.getElementById('name').value = userData.name || ''; 
-                document.getElementById('phone').value = userData.phone || '';
-                document.getElementById('local').value = userData.local || '';
+// firebase.auth().onAuthStateChanged((user) => {
+// const userId = firebase.auth().currentUser?.uid;
 
-                document.getElementById('name').setAttribute('readonly', 'true');
-                document.getElementById('phone').setAttribute('readonly', 'true');
-                document.getElementById('local').setAttribute('readonly', 'true');
+// if (userId) {
+//     console.log("Usuário logado com ID:", userId);
+//     firebase.firestore().collection('users').doc(userId).get()
+//         .then(doc => {
+//             if (doc.exists) {
+//                 const userData = doc.data();
+//                 console.log("Dados do usuário:", userData);
+//                 document.getElementById('name').value = userData.name || ''; 
+//                 document.getElementById('phone').value = userData.phone || '';
+//                 document.getElementById('local').value = userData.local || '';
 
-                setTimeout(() => {
-                  document.getElementById('name').removeAttribute('readonly');
-                  document.getElementById('phone').removeAttribute('readonly');
-                  document.getElementById('local').removeAttribute('readonly');
-                  console.log("Agora os campos estão editáveis.");
-              }, 5000);
+//                 document.getElementById('name').setAttribute('readonly', 'true');
+//                 document.getElementById('phone').setAttribute('readonly', 'true');
+//                 document.getElementById('local').setAttribute('readonly', 'true');
 
-            } else {
-                console.log("Nenhum documento encontrado para este usuário.");
-            }
-        })
-        .catch(error => {
-            console.error("Erro ao buscar dados do usuário: ", error);
-        });
-} else {
-    console.log("Usuário não autenticado.");
-    // Se o usuário não estiver autenticado, exibe uma mensagem
-    document.getElementById('name').value = 'Usuário não autenticado';
-    document.getElementById('phone').value = 'Usuário não autenticado';
-    document.getElementById('local').value = 'Usuário não autenticado';
-}});
+//                 setTimeout(() => {
+//                   document.getElementById('name').removeAttribute('readonly');
+//                   document.getElementById('phone').removeAttribute('readonly');
+//                   document.getElementById('local').removeAttribute('readonly');
+//                   console.log("Agora os campos estão editáveis.");
+//               }, 5000);
+
+//             } else {
+//                 console.log("Nenhum documento encontrado para este usuário.");
+//             }
+//         })
+//         .catch(error => {
+//             console.error("Erro ao buscar dados do usuário: ", error);
+//         });
+// } else {
+//     console.log("Usuário não autenticado.");
+//     // Se o usuário não estiver autenticado, exibe uma mensagem
+//     document.getElementById('name').value = 'Usuário não autenticado';
+//     document.getElementById('phone').value = 'Usuário não autenticado';
+//     document.getElementById('local').value = 'Usuário não autenticado';
+// }});
