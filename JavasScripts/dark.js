e3efc446e81a0e618a7d2c949b5b54d1ff0f4697
Alan Lopes <alan46862546@edu.pa.senac.br>
2025-01-29 23:37:13 -0300

    salva informação de modo escuro/claro

--- JavasScripts/dark.js
+++ JavasScripts/dark.js
@@ -7,11 +7,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const dropdownMenu = document.querySelector('.dropdown_menu');
     const linkLogo = document.querySelector('.navbar .logo a');
 
+    // Recupera a preferência de tema do localStorage
+    const temaSalvo = localStorage.getItem('modoEscuro');
+    if (temaSalvo === 'ativo') {
+        body.classList.add("dark-mode");
+        dropdownMenu.classList.add("dark-mode");
+        fundoMd.style.backgroundColor = '#121212';
+        iconremix.classList.replace('ri-moon-line', 'ri-sun-fill');
+        iconremix.style.color = 'white';
+        toggleBntIcon.style.color = 'white';
+        linkLogo.style.color = 'white';
+    } else {
+        body.classList.remove("dark-mode");
+        dropdownMenu.classList.remove("dark-mode");
+        fundoMd.style.backgroundColor = '#eeeeee';
+        iconremix.classList.replace('ri-sun-fill', 'ri-moon-line');
+        iconremix.style.color = 'black';
+        toggleBntIcon.style.color = 'black';
+        linkLogo.style.color = 'black';
+    }
+
     button.addEventListener("click", () => {
         body.classList.toggle("dark-mode");
         dropdownMenu.classList.toggle("dark-mode");
 
-        // Transições suaves
         body.style.transition = "background-color 1s, color 1s";
         fundoMd.style.transition = "background-color 1s, color 1s";
 
@@ -21,14 +40,17 @@ document.addEventListener('DOMContentLoaded', () => {
             iconremix.style.color = 'white';
             toggleBntIcon.style.color = 'white';
             linkLogo.style.color = 'white';
+            
+            localStorage.setItem('modoEscuro', 'ativo');
         } else {
             fundoMd.style.backgroundColor = '#eeeeee';
             iconremix.classList.replace('ri-sun-fill', 'ri-moon-line');
             iconremix.style.color = 'black';
             toggleBntIcon.style.color = 'black';
             linkLogo.style.color = 'black';
+            
+            localStorage.setItem('modoEscuro', 'inativo');
         }
     });
 });
-
 
